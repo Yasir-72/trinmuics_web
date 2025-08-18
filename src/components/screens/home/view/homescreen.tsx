ecf81cf9178c459963e05870d66dbce3db103555
Yasir <sayedyasirabbas99@gmail.com>
2025-08-18 22:17:14 +0530

    Removed unused components from Page and Homescreen, updated imports accordingly

--- src/components/screens/home/view/homescreen.tsx
+++ src/components/screens/home/view/homescreen.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import HeroSection from "../sections/heroscection";
 import ServiceSection from "../sections/servicesection";
+import WhyChooseUs from "../sections/whysection";
+import HowItWorks from "../sections/howsection";
 
 export default function Homescreen() {
   return (
     <>
       <HeroSection />
       <ServiceSection />
+      <WhyChooseUs />
+      <HowItWorks />
     </>
   );
 }
