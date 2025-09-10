0a1f09f6626b679f44fc12827f600b2fef530192
Yasir-72 <sayedyasirabbas99@gmail.com>
2025-09-10 22:09:10 +0530

    `Refactored JSX in multiple pages to use Fragment instead of unnecessary divs`

--- src/app/Service/page.tsx
+++ src/app/Service/page.tsx
@@ -3,5 +3,8 @@ import React from "react";
 import ServiceScreen from "@/components/screens/service/view/servicescreen";
 
 export default function Page() {
-  return <ServiceScreen />;
+  return;
+  <>
+    <ServiceScreen />
+  </>;
 }
--- src/app/technologies/page.tsx
+++ src/app/technologies/page.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export default function page() {
   return (
-    <div>
-      <><h1>technologies</h1></>
-    </div>
-  )
+    <>
+      <h1>technologies</h1>
+    </>
+  );
 }
