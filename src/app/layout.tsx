adc44bf072b8029737ecdbd230b008f6ae48877f
Nasir Zaidi <72230814+Nasir0020@users.noreply.github.com>
2025-09-27 16:11:08 +0530

    layout changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,10 @@
 "use client";
 
-import { useState } from "react";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
 import Header from "../components/layout/header";
 import Footer from "@/components/layout/footer";
-// import SplashScreen from "@/components/layout/splashscreen";
 import CtaBanner from "@/components/layout/cta";
-// import SplashScreen from "@/components/layout/splashscreen"; // ✅ Import Splash Screen
-// import { useEffect, useState } from "react";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -18,11 +14,8 @@ const montserrat = Montserrat({
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
-  const [loading, setLoading] = useState(true);
-
 }>) {
   return (
     <html lang="en">
@@ -30,21 +23,10 @@ export default function RootLayout({
         suppressHydrationWarning={true}
         className={`${montserrat.variable} antialiased bg-[#DDE2E5] px-2 md:px-10`}
       >
-        {loading ? (
-          <SplashScreen onComplete={() => setLoading(false)} />
-        ) : (
-          <>
-            <Header />
-            {children}
-            <Footer />
-          </>
-        )}
-        <>
-          <Header />
-          {children}
-          <CtaBanner />
-          <Footer />
-        </>
+        <Header />
+        {children}
+        <CtaBanner />
+        <Footer />
       </body>
     </html>
   );
